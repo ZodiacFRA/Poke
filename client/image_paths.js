0c391178bd2d090778277ef5b2c175079b4e39b2
mlarboul <mlarboul@student.42.fr>
2022-11-27 23:31:42 +0100

    add new images

--- client/image_paths.js
+++ client/image_paths.js
@@ -1,13 +1,1 @@
-const images = [
-	"./img/ground.png",
-	"./img/stone_1.png",
-	"./img/lava_0.png",
-	"./img/gravel_0.png",
-	"./img/player.png",
-	"./img/wall.png",
-	"./img/desert_grass_0.png",
-	"./img/sand_0.png",
-	"./img/desert_water_0.png",
-	"./img/stone_0.png",
-	"./img/grass_0.png",
-];
+const images = ["./img/ground.png", "./img/wall.png", "./img/player.png"];
