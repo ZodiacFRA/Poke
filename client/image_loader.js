4edfec5ff7af1d28fe65f1454e435cf2e29d08fa
mlarboul <mlarboul@student.42.fr>
2022-11-27 22:04:34 +0100

    Change indent

--- client/image_loader.js
+++ client/image_loader.js
@@ -1,23 +1,25 @@
 var db_img;
 var missing_load = 10000;
 
-var load_db = function(){
-    db_img = {};
-    missing_load = images.length
-    for (const elem of images) {
-        img_name = elem.split('/').slice(-1)[0].split('.')[0]
+var load_db = function () {
+  db_img = {};
+  missing_load = images.length;
+  for (const elem of images) {
+    img_name = elem.split("/").slice(-1)[0].split(".")[0];
 
-        var tmp_img = new Image()
-        tmp_img.onload = function () {missing_load--;}
-        tmp_img.src = elem;
-        db_img[img_name] = tmp_img;
-    }
+    var tmp_img = new Image();
+    tmp_img.onload = function () {
+      missing_load--;
+    };
+    tmp_img.src = elem;
+    db_img[img_name] = tmp_img;
+  }
 };
 
-const interval = setInterval(function() {
-    if (missing_load != 0){
-        console.log("Missing " + missing_load + " img to load;")
-    }
+const interval = setInterval(function () {
+  if (missing_load != 0) {
+    console.log("Missing " + missing_load + " img to load;");
+  }
 }, 1000);
 
-load_db()
+load_db();
