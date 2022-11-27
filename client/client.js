2154604b1a31c035cf37ebfbbf78c9ab35c754ed
mlarboul <mlarboul@student.42.fr>
2022-11-27 22:05:40 +0100

    Refacto intial code and add messageToServer object

--- client/client.js
+++ client/client.js
@@ -1,79 +1,134 @@
 "restric mode";
 
-// Create websocket connection
-const url = "ws://localhost:50000";
-const connection = new WebSocket(url);
+const imagesPath = ["img/ground.png", "img/wall.png", "img/player.png"];
+const URL = "localhost";
+const PORT = 50000;
+const SCREEN_WIDTH = 720;
+const SCREEN_HEIGHT = 480;
+const PLAYER_ID = "UnDesSix";
+const TILE_SIZE = 16;
 
-// Shoud not be hardcoded
-const pixelSize = 16;
-const mapWidth = 20;
-const mapHeight = 20;
-
-// Get and resize canvas
-const canvas = document.getElementById("viewport");
-// canvas.width = mapWidth * pixelSize;
-// canvas.height = mapHeight * pixelSize;
-
-// Return a drawing context on the canvas
-const c = canvas.getContext("2d");
-
-// Create images and import them
-const groundImg = new Image();
-groundImg.src = "img/ground.png";
-const wallImg = new Image();
-wallImg.src = "img/wall.png";
-const playerImg = new Image();
-playerImg.src = "img/player.png";
-
-// Global variables to store received data (sprite unused for now)
-let mapJson;
-let spriteJson;
-
-// When (and if) a connection is established
-connection.onopen = () => {
-  connection.send("Matthieu");
-  window.addEventListener("keydown", (e) => {
-    connection.send(e.key);
-  });
+const keyboard = {
+  listen: function () {
+    window.addEventListener("keydown", (e) => {
+      server.msgToServer = { type: "key_event", content: "e.key" };
+      server.connection.send(server.msgToServer);
+    });
+  },
 };
 
-connection.onerror = (error) => {
-  console.log(`WebSocket error: ${error}`);
-};
-
-// Each time an update is received, the onmessage event occurs
-connection.onmessage = (e) => {
-  const msg = JSON.parse(e.data);
-  console.log(msg);
-
-  switch (msg.type) {
-    case "init_map":
-      mapJson = msg.map;
-      spriteJson = msg.sprites_table;
-      console.log(msg.sprites_table);
-      drawMap();
+class image {
+  constructor(path) {
+    this.obj = new Image();
+    this.obj.src = path;
+    if (obj.height > TILE_SIZE || obj.width > TILE_SIZE)
+      this.isForeground = true;
   }
-};
-
-var player_pos = [2,2];
-var view_distance = 28;
-
-function drawMap() {
-  var half_disp_width = viewport_shape[0] / 16 / 2;
-  var half_disp_height = viewport_shape[1] / 16 / 2;
-
-  for (let y = Math.floor(player_pos[0] - half_disp_height); y < player_pos[0] + half_disp_height; y++) {
-    if (y < 0 || y >= mapJson.bottom.length)
-      continue
-    for (let x = Math.floor(player_pos[1] - half_disp_width); x < player_pos[1] + half_disp_width; x++) {
-      if (x < 0 || x >= mapJson.bottom[y].length)
-        continue
-      // console.log("dlkajsld;kfj ", mapJson.bottom[y][x], mapJson.top[y][x])
-      c.drawImage(db_img[spriteJson[mapJson.bottom[y][x]]], x * pixelSize - pixelSize / 2 + half_disp_width * 16, y * pixelSize - pixelSize / 2 + half_disp_height * 16);
-      if (mapJson.top[y][x])
-        c.drawImage(db_img[spriteJson[mapJson.top[y][x]]], x * pixelSize - pixelSize / 2 + half_disp_width * 16, y * pixelSize - pixelSize / 2 + half_disp_height * 16);
-    }
-  }
-
-  console.log("\n\n")
 }
+
+const character = {
+  image: () => {
+    return image();
+  },
+};
+
+const game = {
+  map: {
+    content: null,
+    width: 0,
+    height: null,
+    sprites: null,
+  },
+  playerMoved: true,
+
+  movePlayer: function () {
+    this.playerMoved = false;
+    for (let i = 0; i < display.tileSize; i++) {
+      //
+    }
+  },
+};
+
+const server = {
+  connection: new WebSocket("ws://" + URL + ":" + PORT),
+  msgFromServer: null,
+  msgToServer: null,
+
+  connect: function () {
+    this.msgToServer = { type: "new_connection", content: PLAYER_ID };
+    this.connection.onopen = () =>
+      this.connection.send(JSON.stringify(msgToServer));
+  },
+
+  listen: function () {
+    this.connect();
+
+    this.connection.onerror = (error) => {
+      console.log(`WebSocket error: ${error}`);
+    };
+
+    this.connection.onmessage = (event) => {
+      try {
+        this.msgFromServer = JSON.parse(event.data);
+        this.parseMsg();
+      } catch (exception) {}
+    };
+  },
+
+  parseMsg: function () {
+    switch (this.msgFromServer.type) {
+      case "init_map":
+        game.map.content = this.msgFromServer.map;
+        game.map.height = game.map.content.bottom.length;
+        game.map.width = game.map.content.bottom[0].length;
+        game.map.sprites = this.msgFromServer.sprites_table;
+        display.drawMap();
+    }
+  },
+};
+
+const display = {
+  viewport: null,
+  ctx: null,
+  images: [],
+
+  init: function () {
+    this.setViewport();
+    this.setContext();
+    this.loadImages();
+  },
+
+  setViewport: function () {
+    this.viewport = document.getElementById("viewport");
+    this.viewport.width = SCREEN_WIDTH;
+    this.viewport.height = SCREEN_HEIGHT;
+  },
+
+  setContext: function () {
+    this.ctx = this.viewport.getContext("2d");
+  },
+
+  loadImages: function () {
+    for (var i = 0; i < imagesPath.length; i++) {
+      this.images.push(new Image());
+      this.images[i].src = imagesPath[i];
+    }
+  },
+
+  // NEED TP INSERT SEUB FUNCTION INSTEAD
+  drawMap: function () {
+    for (var y = 0; y < game.map.height; y++) {
+      for (var x = 0; x < game.map.width; x++) {
+        // DRAW BACKGROUND
+        this.ctx.drawImage(this.images[1], x * TILE_SIZE, y * TILE_SIZE);
+        // DRAW OBJECT
+
+        // DRAW FOREGROUND
+      }
+    }
+  },
+};
+
+display.init();
+server.listen();
+keyboard.listen();
