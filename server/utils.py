e6bd47216640bf79c908b8f465696e4eef035425
zodiac <jean-baptiste.dupuy@epitech.eu>
2022-12-12 13:13:55 +0100

    loader cleanup

--- server/utils.py
+++ server/utils.py
@@ -5,7 +5,7 @@ from Entities import *
 from HelperClasses import Tile, Position
 
 
-def get_map_from_file(map_filepath):
+def get_map_from_json_file(map_filepath):
     map = []
     with open(map_filepath, "r") as f:
         data = f.read()
@@ -26,13 +26,11 @@ def get_map_from_file(map_filepath):
                 map_line.append(Tile())
             else:
                 if top_entity_info and top_entity_info[0] == "wall":
-                    # print(top_entity_info, y_idx, x_idx)
                     map_line.append(Tile(
                         Ground(Position(y_idx, x_idx), -1),
                         Wall(Position(y_idx, x_idx), top_entity_info[1])
                     ))
                 elif bottom_entity_info and bottom_entity_info[0] == "ground":
-                    # print(bottom_entity_info)
                     map_line.append(Tile(
                         Ground(Position(y_idx, x_idx), bottom_entity_info[1])
                     ))
@@ -40,36 +38,35 @@ def get_map_from_file(map_filepath):
     return map, y_size, x_size
 
 
-#
-# def get_map_from_file(map_filepath):
-#     map = []
-#     with open(map_filepath, "r") as f:
-#         data = f.read().split('\n')
-#         data = list(filter(None, data))
-#
-#     y_size = len(data)
-#     x_size = max(len(x) for x in data)
-#     print(f"[ ] - Map loader: max height detected: {y_size}")
-#     print(f"[ ] - Map loader: max width detected: {x_size}")
-#     for y_idx, line in enumerate(data):
-#         map_line = []
-#         for x_idx, char in enumerate(line):
-#             if char == "":
-#                 map_line.append(Tile())
-#             elif char == "0":
-#                 sprite_idx = random.randint(0, 1)
-#                 map_line.append(Tile(
-#                     Ground(Position(y_idx, x_idx), sprite_idx)
-#                 ))
-#             elif char == "1":
-#                 map_line.append(Tile(
-#                     Ground(Position(y_idx, x_idx)),
-#                     Wall(Position(y_idx, x_idx))
-#                 ))
-#             elif char == "2":
-#                 map_line.append(Tile(
-#                     Ground(Position(y_idx, x_idx)),
-#                     Player(Position(y_idx, x_idx))
-#                 ))
-#         map.append(map_line)
-#     return map, y_size, x_size
+def get_map_from_txt_file(map_filepath):
+    map = []
+    with open(map_filepath, "r") as f:
+        data = f.read().split('\n')
+        data = list(filter(None, data))
+
+    y_size = len(data)
+    x_size = max(len(x) for x in data)
+    print(f"[ ] - Map loader: max height detected: {y_size}")
+    print(f"[ ] - Map loader: max width detected: {x_size}")
+    for y_idx, line in enumerate(data):
+        map_line = []
+        for x_idx, char in enumerate(line):
+            if char == "":
+                map_line.append(Tile())
+            elif char == "0":
+                sprite_idx = random.randint(0, 1)
+                map_line.append(Tile(
+                    Ground(Position(y_idx, x_idx), sprite_idx)
+                ))
+            elif char == "1":
+                map_line.append(Tile(
+                    Ground(Position(y_idx, x_idx)),
+                    Wall(Position(y_idx, x_idx))
+                ))
+            elif char == "2":
+                map_line.append(Tile(
+                    Ground(Position(y_idx, x_idx)),
+                    Player(Position(y_idx, x_idx))
+                ))
+        map.append(map_line)
+    return map, y_size, x_size
