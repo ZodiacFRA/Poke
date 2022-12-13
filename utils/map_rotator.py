754913ef4d6c02f5edea5aaf28e17408129fcd11
ZodiacFRA <jean-baptiste.dupuy@epitech.eu>
2022-12-13 01:25:02 +0100

    PS

--- utils/map_rotator.py
+++ utils/map_rotator.py
@@ -1,15 +1,11 @@
-import numpy as np
 import json
+import numpy as np
 
 with open("./map_gen1.json", "r") as f:
     tmp = json.loads(f.read())
 data = tmp["map"]
-top = np.array(data["top"], dtype=object)
-bottom = np.array(data["bot"], dtype=object)
-print(top)
-print(bottom)
-top = np.rot90(top)
-bottom = np.rot90(bottom)
+top = np.rot90(np.array(data["top"], dtype=object))
+bottom = np.rot90(np.array(data["bot"], dtype=object))
 tmp["map"]["top"] = top.tolist()
 tmp["map"]["bot"] = bottom.tolist()
 with open("./map_gen1_rot.json", "w") as f:
